42a581f0a78256ac53dc0484219d09e20c0b1d17
Vinícius Martins <114368898+Hainverst@users.noreply.github.com>
2025-05-01 22:51:01 -0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,4 @@
+const newAudio = new Audio('./Lana Del Rey - Say Yes To Heaven (Official Audio).mp3')
+document.getElementById('sim').addEventListener('click', function() {
+     newAudio.play()
+  });
